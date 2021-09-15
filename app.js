dd377092601e3f1e2a1580f44d255cbedc819b39
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-09-14 23:35:39 -0400

    create Dino objects manually

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 function Dino(species, weight, height, diet, where, when, fact) {
   this.species = species;
   this.weight = weight;
+  this.height = height;
   this.diet = diet;
   this.where = where;
   this.when = when;
@@ -9,6 +10,76 @@ function Dino(species, weight, height, diet, where, when, fact) {
 }
 
 // Create Dino Objects
+
+let triceratops = new Dino(
+  "Triceratops",
+  13000,
+  114,
+  "herbavor",
+  "North America",
+  "Late Cretaceous",
+  "First discovered in 1889 by Othniel Charles Marsh"
+);
+
+let tyrannosaurusRex = new Dino(
+  "Tyrannosaurus Rex",
+  11905,
+  144,
+  "carnivor",
+  "North America",
+  "Late Cretaceous",
+  "The largest known skull measures in at 5 feet long."
+);
+
+let anklyosaurus = new Dino(
+  "Anklyosaurus",
+  10500,
+  55,
+  "herbavor",
+  "North America",
+  "Late Cretaceous",
+  "Anklyosaurus survived for approximately 135 million years."
+);
+
+let brachiosaurus = new Dino(
+  "Brachiosaurus",
+  7000,
+  372,
+  "herbavor",
+  "North America",
+  "Late Jurasic",
+  "An asteroid was named 9954 Brachiosaurus in 1991"
+);
+
+let stegosaurus = new Dino(
+  "Stegosaurus",
+  11600,
+  79,
+  "herbavor",
+  "North America, Europe, Asia",
+  "Late Jurasic to Early Cretaceous",
+  "The Stegosaurus had between 17 and 22 seperate places and flat spines"
+);
+
+let elasmosaurus = new Dino(
+  "Elasmosaurus",
+  16000,
+  59,
+  "carnivor",
+  "North America",
+  "Late Cretaceous",
+  "Elasmosaurus was a marine reptile first discovered in Kansas"
+);
+
+let pteranodon = new Dino(
+  "Pteranodon",
+  44,
+  20,
+  "carnivor",
+  "North America",
+  "Late Cretaceous",
+  "Actually a flying reptile, the Pteranodon is not a dinosaur"
+);
 
 // Create Human Object
 
